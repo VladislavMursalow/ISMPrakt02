33745dd9eb13409f43fed4278ecb21b32a101828
Mursalow Vlad <ki201_mvr@student.ztu.edu.ua>
2021-10-17 22:03:55 +0300

    Перший коміт
    Додано ругу практичну роботу

---
+++ LinearExpressions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LinearExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d;
+            Console.WriteLine("Введіть число a");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число b");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число c");
+            c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число d");
+            d = double.Parse(Console.ReadLine());
+
+            double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (Math.Pow(a, 2)) / (Math.Pow(b, 2));
+            double y = (5 * (a + b) * (c - d)) / (0.5 * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2) ) / (b - a));
+            double z = ((Math.Pow(Math.Pow(x, 2) - 2 * x, 3) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b)) / (5 * a + 3 * b);
+            double r = (1 / 2 * a + 3 / 4 * b - 7 / 5) / (3 * c + 1) + (1 / a - c);
+            
+            Console.WriteLine($"x:{x}");
+            Console.WriteLine($"y:{y}");
+            Console.WriteLine($"z:{z}");
+            Console.WriteLine($"r:{r}");
+        }
+    }
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Введіть число a");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число b");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введіть число c");
+            c = double.Parse(Console.ReadLine());
+
+            double D = Math.Pow(b, 2) - 4 * a * c;
+
+            if (D > 0)
+            {
+                double x1 = -b + Math.Sqrt(D) / 2 * a;
+                double x2 = -b - Math.Sqrt(D) / 2 * a;
+                Console.WriteLine($"x1: {x1}, x2: {x2}");
+            }
+            else if (D == 0)
+            {
+                double x = -b / 2 * a;
+                Console.WriteLine($"x: {x}");
+
+            }
+            else {
+                Console.WriteLine("D < 0, коренів не існує");
+            }
+        }
+    }
+}
